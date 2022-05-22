64b23344086336bd38156b17bf2bfa1730feb1a2
HabaySecureId <92311631+HabaySecureId@users.noreply.github.com>
2022-05-22 21:52:46 +0100

    ProjectDashBoard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import './App.css'
 
-function App() {
+const App = () => {
+  const activeMenu = true;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+      <div className='flex relative dark:bg-main-dark-bg'>
+<div className="fixed right-4 bottom-4" style={{zIndex: '1000'}}>
+<TooltipComponent content="Settings" position="Top">
+   <button type='button'
+   className='text-3x1 p-3 hover: drop-shadow-x1 hover:bg-light-gray text-white' style={{background:'blue',borderRadius: '50%'}} >
+     <FiSettings/>
+   </button>
+</TooltipComponent>
+</div>
+{activeMenu ? (
+  <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
+    Sidebar
+  </div>
+) : (
+  <div>
+    Sidebar w-0
+  </div>
+)}
+</div>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
