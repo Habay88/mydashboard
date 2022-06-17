85de8650bcfabc57bfae41f19a487b6868612376
HabaySecureId <92311631+HabaySecureId@users.noreply.github.com>
2022-06-17 20:33:52 +0100

    updated App component for page allocation

--- src/component/index.jsx
+++ src/component/index.jsx
@@ -1,8 +1,8 @@
 export { default as Button } from './Button';
 export { default as ThemeSettings } from './ThemeSettings';
-export { default as Sidebar } from './Sidebar';
-// eslint-disable-next-line import/no-cycle
-export { default as Navbar } from './Navbar';
+export { default as SideBar } from './SideBar';
+
+export { default as NavBar} from './NavBar';
 export { default as Footer } from './Footer';
 export { default as Cart } from './Cart';
 export { default as Chat } from './Chat';
