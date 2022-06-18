f936a89bc757e4a4702583824d8af4240d0a3139
HabaySecureId <92311631+HabaySecureId@users.noreply.github.com>
2022-06-18 23:10:17 +0100

    completed dashboard with little bug to fix at navbar

--- src/pages/Charts/Area.jsx
+++ src/pages/Charts/Area.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, SplineAreaSeries,LineSeries, DateTime, Legend, Tooltip } from '@syncfusion/ej2-react-charts';
 
+import { areaCustomSeries, areaPrimaryYAxis, areaPrimaryXAxis } from '../../data/dummy';
+import { useStateContext } from '../../contexts/ContextProvider';
+import { Header } from '../../component';
 const Area = () => {
-  return (
-    <div>Area</div>
-  )
-}
+  const { currentMode } = useStateContext();
 
-export default Area
+  return (
+    <div>
+
+    
+    <ChartComponent
+      id="line-chart"
+      height="420px"
+      primaryXAxis={areaPrimaryXAxis}
+      primaryYAxis={areaPrimaryYAxis}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+      legendSettings={{ background: 'white' }}
+    >
+      <Inject services={[SplineAreaSeries, DateTime, Legend]} />
+      <SeriesCollectionDirective>
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        {areaCustomSeries.map((item, index) => <SeriesDirective key={index} {...item} />)}
+      </SeriesCollectionDirective>
+    </ChartComponent>
+    </div>
+  );
+};
+
+export default Area;
